b6d4e17aecf77ce435009088aba2ae42d925b1b9
aibin2008 <aibin2008@hotmail.com>
2021-03-10 15:15:32 +0800

    fix mock

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -6,9 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
 
-    constructor(string memory name, string memory symbol, uint8 _decimal) public ERC20(name, symbol) {
-        _setupDecimals(_decimal);
-        _mint(msg.sender, 1000000000*10**_decimal);
+    constructor(string memory name, string memory symbol, uint8 _decimal_) public ERC20(name, symbol) {
+        _setupDecimals(_decimal_);
+        _mint(msg.sender, 1000000000*10**uint(_decimal_));
     }
 
 }
